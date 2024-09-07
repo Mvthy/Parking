a4a52e8f339bc7f85b14cc69a90cf73d7c76f8a3
Zzenit <2001juan.bravo@gmail.com>
2024-09-07 12:51:28 -0400

    se creo page recuperar
    se crea page recuperar para recuperar contraseña

--- ParkingApp/src/app/app.module.ts
+++ ParkingApp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Page/login/login.component';
+import { RecuperarComponent } from './Page/recuperar/recuperar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RecuperarComponent
   ],
   imports: [
     BrowserModule,
