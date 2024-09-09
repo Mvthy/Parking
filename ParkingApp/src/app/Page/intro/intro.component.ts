b6deef6376b295bcc57ecff73b9cde5d8ff8ca48
Mvthy <mathystark17@gmail.com>
2024-09-09 17:55:29 -0300

    Creacion de intro
    Se crea y personaliza la pantalla intro

--- ParkingApp/src/app/Page/intro/intro.component.ts
+++ ParkingApp/src/app/Page/intro/intro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-intro',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './intro.component.css'
 })
 export class IntroComponent {
-
+  constructor(public router: Router) {
+    setTimeout(() => {
+      this.router.navigateByUrl('login')
+    }, 4000);
+  }
 }
--- ParkingApp/src/app/app-routing.module.ts
+++ ParkingApp/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Page/login/login.component';
+import { IntroComponent } from './Page/intro/intro.component';
 
 const routes: Routes = [
 
   {
     path: 'login',
     component: LoginComponent
+  },
+
+  {
+    path: 'intro',
+    component: IntroComponent
   }
 
 ];
