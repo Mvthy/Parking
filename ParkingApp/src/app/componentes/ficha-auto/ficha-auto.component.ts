ff326c7f761a0bee1491a73f23d3d62fd3d40c3c
Mvthy <mathystark17@gmail.com>
2024-11-05 12:15:42 -0300

    Modificacion ficha-auto
    Se modifica ficha-auto para mejor busqueda en campos

--- ParkingApp/src/app/componentes/ficha-auto/ficha-auto.component.ts
+++ ParkingApp/src/app/componentes/ficha-auto/ficha-auto.component.ts
@@ -24,6 +24,43 @@ export class FichaAutoComponent {
     matricula: ''
   };
 
+  // Lista completa de marcas
+  marcas = ['Toyota', 'Volkswagen', 'Ford', 'Honda', 'Nissan', 'Chevrolet', 
+    'BMW', 'Mercedes-Benz', 'Hyundai', 'Audi', 'Kia', 'Lexus', 'Peugeot', 
+    'Renault', 'Subaru', 'Mazda', 'Jeep', 'Dodge', 'Tesla', 'Mitsubishi', 
+    'Land Rover', 'Porsche', 'Fiat', 'Volvo', 'Jaguar', 'Chrysler', 'Ram', 
+    'Mini', 'Acura', 'Buick', 'Cadillac', 'Lincoln', 'Alfa Romeo', 'Maserati', 
+    'Ferrari', 'Lamborghini', 'Aston Martin', 'Infiniti', 'Suzuki', 'Genesis', 
+    'Seat', 'Skoda', 'Citroën', 'GMC', 'Haval', 'Mahindra', 'Bentley', 
+    'Rolls-Royce', 'SsangYong', 'McLaren'];
+
+  colores = ['Blanco', 'Negro', 'Gris', 'Plateado', 'Azul', 'Rojo', 'Verde', 'Cafe',
+    'Marrón', 'Beige', 'Amarillo', 'Naranja', 'Violeta', 'Turquesa', 'Cyan', 'Rosado'];
+
+  marcasFiltradas = this.marcas;
+  coloresFiltrados = this.colores;
+
+  filtrarMarcas() {
+    this.marcasFiltradas = this.marcas.filter(marca =>
+      marca.toLowerCase().startsWith(this.auto.marca.toLowerCase())
+    );
+  }
+
+  filtrarColores() {
+    this.coloresFiltrados = this.colores.filter(color =>
+      color.toLowerCase().startsWith(this.auto.color.toLowerCase())
+    );
+  }
+
+  // Método para validar la matrícula
+  validarMatricula() {
+    const regex = /^[a-zA-Z0-9]*$/; // Solo permite letras y números
+    if (!regex.test(this.auto.matricula)) {
+      // Remueve el último carácter si es inválido
+      this.auto.matricula = this.auto.matricula.slice(0, -1);
+    }
+  }
+
   guardar() {
     console.log('Datos guardados:', this.auto);
     // Aquí puedes agregar la lógica para guardar los datos, como enviar a un servicio
