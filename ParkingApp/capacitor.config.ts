28b21dfd430d5e462355a98a156fe60228f396be
thalaya <134561839+THALIA2308@users.noreply.github.com>
2024-12-07 14:06:58 -0300

    Create capacitor.config.ts
    apk

---
+++ ParkingApp/capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.parking.app',
+  appName: 'parking-appHighBridge',
+  webDir: 'dist/parking-app/browser'
+  
+};
+
+export default config;
